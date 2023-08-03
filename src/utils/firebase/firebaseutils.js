a780cd0266aabc06b1776e6d41759fb02d3358f6
oluwafemioluwatuyi <oluwafemioluwatuyi8@gmail.com>
2023-08-03 11:25:01 +0100

    update sign in and firebase component

--- src/utils/firebase/firebaseutils.js
+++ src/utils/firebase/firebaseutils.js
@@ -4,6 +4,13 @@ import {getAuth,
         signInWithPopup,
         GoogleAuthProvider} from 'firebase/auth'
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore'
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyAdiiiO8Wg4CZGw6zSYnMB2Pnp7PtVAhVY",
@@ -19,6 +26,44 @@ const firebaseConfig = {
 
   const provider = new GoogleAuthProvider();
 
+  
+
   provider.setCustomParameters({
     promp: "select_account"
   })
+  
+  export const auth = getAuth();
+  export const signInWithGooglePopup =() => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+
+   export const createUserDocumentFromAuth = async(userAuth) =>{
+
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot)
+    console.log(userSnapshot.exists())
+
+    if(!userSnapshot.exists()){
+      const {displayName, email} = userAuth;
+      const createAt = new Date()
+
+      try{
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createAt
+        })
+      } catch(error){
+        console.log('error creating the user', error.message)
+      }
+    }
+
+    return userDocRef;
+
+
+  }
