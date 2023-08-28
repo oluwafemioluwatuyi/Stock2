6b34243305ec927dcddbbfb7115390e425873a58
oluwafemioluwatuyi <oluwafemioluwatuyi8@gmail.com>
2023-08-28 10:53:37 +0100

    added addToCart feature

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,4 +1,5 @@
-import {useState, createContext} from 'react';
+import {useState, createContext, useEffect
+} from 'react';
 
 
 const addCartItem = (cartItems, productToAdd) => {
@@ -31,8 +32,8 @@ export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
-    addItemToCart: () => {}
-
+    addItemToCart: () => {},
+    cartCount: 0,
 
 });
 
@@ -40,12 +41,20 @@ export const CartProvider = ({children}) =>{
 
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount]= useState(0);
+
+    useEffect(() => {
+
+        const newCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity , 0)
+        setCartCount(newCount)
+
+    }, [cartItems])
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
 
     }
-    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart}
+    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount}
     return(
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     ) 
