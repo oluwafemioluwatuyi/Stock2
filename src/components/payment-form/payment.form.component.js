58d0c263d9162490f192aab72c6c773d321f2190
Oluwafemi <olumifemi2016@gmail.com>
2024-05-20 08:36:52 +0100

    stripe gateway setup

--- src/components/payment-form/payment.form.component.js
+++ src/components/payment-form/payment.form.component.js
@@ -1,45 +1,43 @@
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import {PaymentFormContainer, FormContainer} from './payment-form.styles'
 
-import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component'
+import { useState } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { useSelector } from 'react-redux';
+import { FormContainer } from './payment-form.styles';
+import { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import { PaymentButton, PaymentFormContainer } from './payment-form.styles';
 
-const PaymentForm =()=>{
-    const stripe = useStripe();
-    const elements = useElements();
-
-     // don't want to let default form submission happen here,
-    // which would refresh the page.
-    const paymentHandler = async (e) =>{
-        e.preventDefault();
-
-        // Stripe.js hasn't yet loaded.
-        // disable form submission until Stripe.js has loaded.
-        if(!stripe || !elements){
-            return;
-        }
-
-        const response = await fetch('/.netlify/functions/create-payment-intent',{
-            method: 'post',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body:JSON.stringify({amount:10000})
-        }).then(res => res.json());
-
-        console.log(response);
-
-        
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
     }
-    return(
-       <PaymentFormContainer>
-        <FormContainer onSubmit={paymentHandler}>
-            <h2>Credit Card Payment:</h2>
-         <CardElement/>
-         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
-        </FormContainer>
-       </PaymentFormContainer>   
-    )
+   // setIsProcessingPayment(true);
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 900 * 100 }),
+    }).then((res) => {
+      return res.json();
+    });
+};
 
-}
-
+  return (
+    <PaymentFormContainer>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>Credit Card Payment:</h2>
+        <CardElement />
+        <PaymentButton
+          buttonType={BUTTON_TYPE_CLASSES.inverted}
+        >
+          Pay Now
+        </PaymentButton>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
 export default PaymentForm;
