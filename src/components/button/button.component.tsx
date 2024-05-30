cfea86d9d6aedbe16885f9aa11fb999a489089ae
Oluwafemi <olumifemi2016@gmail.com>
2024-05-30 12:16:35 +0100

    added typescript to component

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,3 +1,4 @@
+import {ButtonHTMLAttributes, FC, HTMLAttributes} from 'react';
 import {
     BaseButton,
     GoogleSignInButton,
@@ -5,20 +6,25 @@ import {
     LoadingSpinner,
   } from "./button.styles";
   
-  export const BUTTON_TYPE_CLASSES = {
-    base: "base",
-    google: "google-sign-in",
-    inverted: "inverted",
+  export enum BUTTON_TYPE_CLASSES {
+    base= "base",
+    google= "google-sign-in",
+    inverted= "inverted",
   };
   
-  const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+  const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseButton =>
     ({
       [BUTTON_TYPE_CLASSES.base]: BaseButton,
       [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
       [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
     }[buttonType]);
+
+  export type buttonProps={
+    buttonType?: BUTTON_TYPE_CLASSES;
+    isLoading?: boolean;
+  } & ButtonHTMLAttributes<HTMLButtonElement>
   
-  const Button = ({ children, buttonType, isLoading = false, ...otherProps }) => {
+  const Button: FC<buttonProps> = ({ children, buttonType, isLoading = false, ...otherProps }) => {
     const CustomButton = getButton(buttonType);
     return (
       <CustomButton disabled={isLoading} {...otherProps}>
