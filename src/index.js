ceb9195403b2934d3fe967f6107549115c8f1334
Oluwafemi <olumifemi2016@gmail.com>
2024-06-06 17:22:27 +0100

    optimization with usememo and usecallback

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Elements } from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -25,7 +27,9 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
